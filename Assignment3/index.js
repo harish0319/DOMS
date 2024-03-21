22738fadae1dd5ef28ac61300d29134f62f6bf8b
hbehera27 <harishb2727@gmail.com>
2024-03-21 11:14:40 +0530

    firstcode

---
+++ Assignment3/index.js
@@ -0,0 +1,13 @@
+const mainh = document.getElementById('main-heading');
+mainh.textContent = 'Fruit World';
+mainh.style.color = 'orange';
+
+const head = document.getElementById('header');
+head.style.backgroundColor = 'green';
+head.style.borderBottom = '3px solid orange';
+
+const bottom = document.getElementById('basket-heading');
+bottom.style.color = 'green';
+
+const thanx = document.getElementById('thanks');
+thanx.innerHTML = '<p>Please visit us again</p>';
